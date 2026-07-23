21b6a5febd6b6438a460d53185cc893ad251a35d
Ginger <ekechukwuma20@gmail.com>
2026-07-23 12:03:31 +0100

    feat: created the handler for the create user

---
+++ internal/handlers/user_handler.go
@@ -0,0 +1,58 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"todo_api/internal/models"
+	"todo_api/internal/repository"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/crypto/bcrypt"
+)
+
+
+type CreateUserHandlerType struct {
+
+	Email string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func CreateUserHandler(pool *pgxpool.Pool ) gin.HandlerFunc {
+	return func (c *gin.Context){
+		
+		var input CreateUserHandlerType;
+
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to bind JSON"})
+			fmt.Sprintln(err.Error())
+			return 
+		}
+
+		if len(input.Password) < 6 {
+			c.JSON(http.StatusBadRequest, gin.H{"error":  " Password must be more than 6 characters!"})
+			return 
+		}
+
+		hashedpassword,err := bcrypt.GenerateFromPassword([]byte(input.Password),bcrypt.DefaultCost )
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+			fmt.Sprintln(err.Error())
+		}
+
+		users := &models.User{
+			Email: input.Email,
+			Password: string(hashedpassword),
+		}
+
+		user,err := repository.CreateUser(pool, users)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": " failed to create user"})
+			fmt.Sprintf(err.Error())
+		}
+
+		c.JSON(http.StatusOK, gin.H{ "User Created successfully": user})
+	}
+}
--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,2 +1,74 @@
 package repository
 
+import (
+	"context"
+	"time"
+	"todo_api/internal/models"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CreateUser(pool *pgxpool.Pool, user *models.User)( *models.User, error){
+	ctx,cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var query string = `INSERT INTO users (email, password) VALUES ($1,$2) RETURNING id, email,creates_at, updated_at`
+
+	err := pool.QueryRow(ctx, query, user.Email, user.Password).Scan(
+		&user.ID,
+		&user.Email,
+		&user.Password,
+		&user.Created_at,
+		&user.Updated_at,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+func GetUserByEmail(pool *pgxpool.Pool, email string)( *models.User, error){
+	ctx,cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var query string = `SElECT id, email, password, created_at, updated_at FROM users WHERE email = $1`
+
+	var user models.User
+
+	err := pool.QueryRow(ctx, query, user.Email, user.Password).Scan(
+		&user.ID,
+		&user.Email,
+		&user.Password,
+		&user.Created_at,
+		&user.Updated_at,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+func GetUserById(pool *pgxpool.Pool, id string)( *models.User, error){
+	ctx,cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var query string = `SElECT id, email, password, created_at, updated_at FROM users WHERE id = $1`
+
+	var user models.User
+
+	err := pool.QueryRow(ctx, query, user.Email, user.Password).Scan(
+		&user.ID,
+		&user.Email,
+		&user.Password,
+		&user.Created_at,
+		&user.Updated_at,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
